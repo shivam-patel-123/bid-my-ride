806d7461262c9a519bf306ddba2b2c616ede320c
snpatel <sh732170@dal.ca>
2023-04-01 11:32:32 -0300

    changed static directory path

--- server/src/controllers/listingController.js
+++ server/src/controllers/listingController.js
@@ -22,7 +22,7 @@ const storage = multer.diskStorage({
         console.log(file);
         // const path = p.join(__dirname, `../../public/assets/images/cars/${req.body.vin}`);
         console.log(process.cwd());
-        const path = path.join(process.cwd(), "public", "assets", "images", "cars", `${req.body.vin}`);
+        const path = path.join(process.cwd(), "server", "public", "assets", "images", "cars", `${req.body.vin}`);
         // const path = `../../public/assets/images/cars/${req.body.vin}`;
         fs.mkdirSync(path, { recursive: true });
         cb(null, `public/assets/images/cars/${req.body.vin}`);
